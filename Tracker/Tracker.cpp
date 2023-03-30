2f18c16535796f5d4538bd97eb184ae2bb674552
OskarFreestyle <oscafe01@ucm.es>
2023-03-30 17:56:41 +0200

    Creado el singleton Tracker
    No esta acabado porque hay que hacer IPersistence y ITrackerAsset antes

--- Tracker/Tracker.cpp
+++ Tracker/Tracker.cpp
@@ -1,9 +1,46 @@
 #include "Tracker.h"
+#include <iostream>
+
+// Variable estatica para el singleton
+Tracker* Tracker::_instance = nullptr;
 
 Tracker::Tracker()
 {
+    std::cout << "Tracker construido con exito" << std::endl;
 }
 
 Tracker::~Tracker()
+{
+    std::cout << "Tracker destruido con exito" << std::endl;
+}
+
+void Tracker::Init()
+{
+    std::cout << "Tracker Init" << std::endl;
+}
+
+void Tracker::End()
+{
+    // Comprueba que el tracker no ha sido borrado antes y lo borra
+    if (_instance != nullptr) {
+        delete _instance;
+        _instance = nullptr;
+    }
+}
+
+Tracker& Tracker::GetInstance()
+{
+    // Crea uno si es el primero
+    if (_instance == nullptr) {
+        _instance = new Tracker();
+        std::cout << "Tracker creado con exito" << std::endl;
+    }
+
+    std::cout << "Tracker ya existente" << std::endl;
+
+    return *_instance;
+}
+
+void Tracker::trackEvent()
 {
 }
--- Tracker/Tracker.h
+++ Tracker/Tracker.h
@@ -1,12 +1,43 @@
 #pragma once
-#include <iostream>
 
 class Tracker
 {
-public:
-	Tracker();
-	~Tracker();
-
 private:
-	std::string id_;
+    Tracker();
+    ~Tracker();
+
+    // Variables
+    // Puntero a la instancia estatica del singleton
+    static Tracker* _instance;
+
+    // TODO add persistenceObjects IPersistence
+    // TODO add activeTrackers List<ITrackerAsset>
+
+public:
+    /// <summary>
+    /// Inicializa el tracker
+    /// </summary>
+    void Init();
+
+    /// <summary>
+    /// Libera los recursos del tracker, persistiendo antes todos los eventos de la cola
+    /// </summary>
+    void End();
+
+    /// <summary>
+    /// Devuelve la instancia del singleton
+    /// </summary>
+    static Tracker& GetInstance();
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    void trackEvent();
+
+    // Metodos eliminados por seguridad del patron singleton
+    // Eliminado el operador de clon
+    Tracker(Tracker& other) = delete;
+    // Eliminado el operador de asignacion
+    void operator=(const Tracker&) = delete;
 };
+
